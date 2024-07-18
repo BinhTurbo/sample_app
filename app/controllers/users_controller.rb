7821d44a78d08705410bbce03f0657a2f9b81344
BinhTurbo <buibinh2200@gmail.com>
2024-07-18 22:28:50 +0700

    Chapter 8

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def index; end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    return if @user
+
+    flash[:warning] = t("user_not_found")
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = t("user_created")
+      redirect_to @user
+    else
+      respond_to do |format|
+        format.html{render :new, status: :unprocessable_entity}
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            @user,
+            partial: "users/form",
+            locals: {user: @user}
+          )
+        end
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
