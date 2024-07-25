f4864a48a13f9eb15487d8c68f7efd48ca9b771c
BinhTurbo <buibinh2200@gmail.com>
2024-07-25 20:05:59 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   before_save :downcase_email
+  attr_accessor :remember_token
 
   validates :name, presence: true, length: {maximum: 50}
   validates :email,
@@ -51,13 +52,33 @@ class User < ApplicationRecord
     email.downcase
   end
 
-  # Returns the hash digest of the given string.
-  def self.digest(string)
-    cost = if ActiveModel::SecurePassword.min_cost
-      BCrypt::Engine::MIN_COST
-    else
-      BCrypt::Engine.cost
+  class << self
+    # Returns the hash digest of the given string.
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+      BCrypt::Password.create(string, cost: cost)
     end
-    BCrypt::Password.create(string, cost: cost)
+
+    def remember
+      self.remember_token = User.new_token
+      update_column :remember_digest, User.digest(remember_token)
+    end
+
+    def authenticated? remember_token
+      BCrypt::Password.new(remember_digest).is_password? remember_token
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Forgets a user.
+  def forget
+    update_column :remember_digest, nil
   end
 end
