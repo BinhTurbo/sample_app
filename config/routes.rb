360df74879b10dca896a564551c77679767db303
BinhTurbo <buibinh2200@gmail.com>
2024-07-25 11:14:23 +0700

    Chap8

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,30 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'demo_partials/new'
-  get 'demo_partials/edit'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get "products/index"
+  get "products/show"
+  get "products/new"
+  get "products/edit"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "demo_partials/new"
+  get "demo_partials/edit"
+
+  get "static_pages/home"
+  get "static_pages/help"
+
+  # Định nghĩa các route cho users
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
+
+  # Định nghĩa các route cho products
+  resources :products
+
+  # Định nghĩa root path
+  root "static_pages#home" # Hoặc trang nào bạn muốn đặt làm trang chính
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
