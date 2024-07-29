ea43ac5a07a4746cba3f5c68313c21300424f952
BinhTurbo <buibinh2200@gmail.com>
2024-07-30 02:11:22 +0700

    Fix code scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  include Pagy::Backend
+  before_action :logged_in_user, except: %i(new create)
+  before_action :find_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
 
-  def index; end
+  def index
+    @pagy, @users = pagy(User.ordered_by_creation, items: Settings.page_10)
+  end
 
   def show
     return if @user
@@ -13,6 +19,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -37,7 +45,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      # Handle a successful update
+      flash[:success] = t "Profile updated"
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "User deleted"
+    else
+      flash[:danger] = t "Delete fail!"
+    end
+    redirect_to users_path
+  end
+
   private
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+  end
 
   def set_user
     @user = User.find_by(id: params[:id])
@@ -50,5 +80,28 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+    redirect_to root_path unless @user
+  end
+
+  # Before filters
+  # Confirms a logged-in user
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "Please log in."
+    redirect_to login_url
+  end
+
+  # Confirms the correct user
+  def correct_user
+    return if current_user?(@user)
+
+    flash[:error] = t "You cannot edit this account."
+    redirect_to root_url
   end
 end
